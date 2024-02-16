c9829406873e9601185290f24eb549b960a9b9a6
jasmina8835 <sunnatovajasmina8002@gmail.com>
2024-02-16 19:54:29 +0500

    newtopics

---
+++ script.js
@@ -0,0 +1,97 @@
+// let str = "salom";
+
+// function sliceFunc(str1, age, ball) {
+    // ? includes()
+
+    // let incStr = str.includes("nima")
+    // ? indexOf
+    // let indexStr = str1.indexOf("dunyo")
+    // let arrFruit = ["banana", "apple", "orange", "lemon"]
+    // let indexArr = arrFruit.indexOf("apple")
+
+    // ? if else shart operatori
+    // if(str1.length > 10) {
+    //     return "string uzuun";
+    // } else {
+    //     return "katta"
+    // }
+
+    // if (age < 16) {
+    //     return "uzur yoshingiz kichik";
+    // } else if(age >= 50) {
+    // } else {
+    //     return "marhamat hush kelibsiz"
+    // }
+
+
+
+    // 90+   A+
+    // 70 va 90 oralig'ida A
+    // 60 va 70 oralig'ida B
+    // 50 va 60 oralig'ida C
+    // 50 dan kichik F  yani yiqilgan
+
+
+//     let score;
+    
+//     if (ball >= 90) {
+//         score = "Darajangiz A+"
+//     } else if (ball < 90 && ball > 70){
+//         score = "Darajangiz A";
+//     }else if (ball >= 70 && ball < 60) {
+//         score = "Darajangiz B"
+//     }else if (score < 50) {
+//         score = "Darajangiz C"
+//     }else if (score < 50) {
+//         score = "Darajangiz F"
+//     }
+
+
+//     let str = ""
+
+//     if(str.length > 1) {
+//     return false
+//     } else {
+//         return true
+//     }
+
+    
+    
+
+// }
+
+// console.log(sliceFunc(str, 50, 87));
+
+
+
+
+
+
+
+
+
+// function sliceFunc(str, num) {
+    
+//     return str.slice(num)
+// }
+
+
+//     console.log(sliceFunc("Robin Singth", 6));
+
+
+
+
+    function capitalize(str, num) {
+        let a = "j";
+        let b = "s string exercises";
+        let c = capitalize.concat(a, b)
+        
+    }
+    
+    
+    console.log(capitalize.toUppercase(a));
+
+
+
+
+
